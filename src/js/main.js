b464411af808d27ca4667c1498b5928914901fbf
Artem Safin <artemsafin.webdev@gmail.com>
2019-06-11 18:51:51 +0300

    Изменены стили

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,21 @@ import objectFitImages from 'object-fit-images';
 import 'simplebar';
 import PerfectScrollbar from 'perfect-scrollbar';
 import Inputmask from 'inputmask';
+import detectIt from 'detect-it';
 
 document.addEventListener('DOMContentLoaded', function() {
   // Полифилл .contains для IE 11
 
   if (!SVGElement.prototype.contains) {
     SVGElement.prototype.contains = HTMLDivElement.prototype.contains;
+  }
+
+
+  // Определение тач устройств
+
+  if (detectIt.hasTouch) {
+    document.body.classList.remove('no-touch')
+    document.body.classList.add('touch')
   }
 
   // Полифилл для CSS свойства ObjectFit(заполнение контейнера изображением)
